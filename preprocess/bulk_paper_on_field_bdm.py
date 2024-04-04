3bc64774c9627887fe8635105fee449d05c2488c
furkan kocak <kocakberatfurkan@gmail.com>
2024-04-04 22:23:40 +0200

    5-2-100 paper find bdm

--- preprocess/bulk_paper_on_field_bdm.py
+++ preprocess/bulk_paper_on_field_bdm.py
@@ -1,11 +1,21 @@
 import requests
 import time
+import json
+
 #url = "https://api.semanticscholar.org/graph/v1/paper/649def34f8be52c8b66281af98ae884c09aef38b"
 url = "https://api.semanticscholar.org/graph/v1/paper/search"
 url_bulk = "https://api.semanticscholar.org/graph/v1/paper/search/bulk"
 api_key = "1R3UkH1BdY6QtZr1wUUtw65hU2bWHe8T69Pq1VFT"
 # Define headers with API key
 headers = {'X-API-KEY': api_key}
+
+top_subjects_dict = {
+    "Computer Science": ["Machine Learning", "Artificial Intelligence"],
+    "Chemistry": ["Organic", "Physical"],
+    "Biology": ["Genetics", "Ecology"],
+    "Business": ["Management", "Marketing"],
+    "Law": ["Criminal", "International"]
+}
 
 query_params = {
     'fields': 'title,paperId' ,
@@ -17,47 +27,67 @@ query_params = {
     'isOpenAccess':'true',
 }
 
+study_fields = top_subjects_dict.keys()
+
 def search_papers(query_params):
     url_bulk = "https://api.semanticscholar.org/graph/v1/paper/search/bulk"
     search_response = requests.get(url_bulk, headers=headers, params=query_params)
     return search_response.json()
 
 
-def fetch_all_results_on_topic(query_params,amount = 1000,field = 'Machine Learning'):
+def fetch_all_results_on_topic(query_params,amount = 1000):
     all_results = []
-    papers_in_field = 0
-    while True:
-        
-        if (papers_in_field >= amount):
-            break
-        response = search_papers(query_params)
-        for result in response['data']:
-        
-            if(result['title'].find(field) != -1):            
-                papers_in_field += 1
-                all_results.append({'paperId': result['paperId'], 'title': result['title']})
-       # all_results.extend(response['data'])
-        if 'token' not in response:
-            break
-        query_params['token'] = response['token']
-        #latency of 0.5 seconds
-        #time.sleep(0.5)
+   
+
+    #create query parameters for each subject in each fields
+    for field in study_fields:
+        query_params['fieldsOfStudy'] = field
+        for top_subject in top_subjects_dict[field]:
+            
+            print(f"Fetching papers for {field} - {top_subject}...")
+
+            papers_in_field = 0
+            while True:
+                
+
+                if (papers_in_field >= amount):
+                    filename = f"matched_papers_{field}_{top_subject}.json"
+                    # Write the data to the JSON file
+                    #path is in bdm_data folder
+                    with open(f"bdm_data/{filename}", 'w') as json_file:
+                        json.dump(all_results, json_file, indent=2)
+                    all_results = []
+                    break
+                response = search_papers(query_params)
+                if 'data' not in response:
+                    break
+                for result in response['data']:
+                
+                    if(result['title'].find(top_subject) != -1):            
+                        papers_in_field += 1
+                        if(papers_in_field >= amount):
+                            break
+                        all_results.append({'paperId': result['paperId'], 'title': result['title']})
+                # all_results.extend(response['data'])
+                if 'token' not in response:
+                    break
+                query_params['token'] = response['token']
+                #latency of 0.5 seconds
+                #time.sleep(0.5)
+            print(f"Matched paper count for {field} - {top_subject}: {papers_in_field}")
+            #remove token
+            query_params.pop('token', None)
     return all_results
 
-field = 'Machine Learning'
-all_result = fetch_all_results_on_topic(query_params, 10, field)
-matched_paper_count = len(all_result)
-print("Matched paper count: ", matched_paper_count)
 
-print("Paper Id  |  Title")
-for result in all_result:
-    print(result['paperId'], " | ", result['title'])
+all_result = fetch_all_results_on_topic(query_params, 100)
+# matched_paper_count = len(all_result)
+# print("Matched paper count: ", matched_paper_count)
 
-#save the results to a json file
-import json
+# print("Paper Id  |  Title")
+# for result in all_result:
+#     print(result['paperId'], " | ", result['title'])
 
 
-filename = f"matched_papers_on_field_{field}_bdm.json"
-# Write the data to the JSON file
-with open(filename, 'w') as json_file:
-    json.dump(all_result, json_file, indent=2)
+
+
